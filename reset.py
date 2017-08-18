020b4b5e97c25ae98b128c439a28763c5724663c
Nick Ginther <nginth@gmail.com>
2017-08-18 12:15:26 -0500

    Revert "reset.py made safer"
    This reverts commit 052881aa1074218e53ffa655e715d1c9d40bd841.

--- reset.py
+++ reset.py
@@ -1,11 +1,6 @@
 from luminance.database import reset_db
 from traceback import print_exc
 try:
-    yn = input('Are you sure you want to reset? y/n')
-    if input == 'n'
-        print('okay, database unchanged')
-        return
-    print('resetting database...')
     reset_db()
     print('done.')
 except:
