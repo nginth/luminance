a2f961fc9698a4a39502c3b1ee55acf3fe215c37
Nick Ginther <nginth@gmail.com>
2017-08-18 12:18:15 -0500

    make reset script safer

--- reset.py
+++ reset.py
@@ -1,7 +1,16 @@
 from luminance.database import reset_db
 from traceback import print_exc
-try:
-    reset_db()
-    print('done.')
-except:
-    print_exc()
+
+def reset():
+    try:
+        yn = input('Are you sure you want to reset the database? This will delete all data and tables. y/n ')
+        if yn == 'n':
+            print('Okay, database unchanged.')
+            return
+        print('Resetting database...')
+        reset_db()
+        print('Done.')
+    except:
+        print_exc()
+
+reset()
