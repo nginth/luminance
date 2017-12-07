00ab1744f456316dc35a70853ca16bcc3ca61ddf
Nick Ginther <nginth@gmail.com>
2017-12-06 20:20:58 -0600

    make run script actually run server

--- run_flask.sh
+++ run_flask.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-source venv/bin/activate;
-export FLASK_APP="luminance/luminance.py";
-export FLASK_DEBUG=1;
+#!/usr/bin/env bash
+. venv/bin/activate
+export FLASK_APP="luminance/luminance.py"
+export FLASK_DEBUG=1
 flask run
